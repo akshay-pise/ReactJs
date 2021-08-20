8c27b033724de3202d5bb85f90282f57ce3ee3f8
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2021-08-20 20:34:42 +0530

    Add files via upload

---
+++ Program1/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<>
+<h1>welcome to react js programing</h1>
+<p>react use language list</p>
+<ul>
+    <li>HTML5</li>
+    <li>CSS</li>
+    <li>Bootstrap</li>
+</ul>
+    </>,document.getElementById('root'));
